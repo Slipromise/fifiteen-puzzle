4fc4c4d0943dbd63810fb5e9f4eeeb526d71c681
Slipromise <chengchun05@gmail.com>
2025-06-26 11:18:46 +0800

    TASK-1223 refactor: improve Matrix component structure and styling

--- src/containers/Matrix.tsx
+++ src/containers/Matrix.tsx
@@ -1,4 +1,3 @@
-
 import { useSelector } from "react-redux";
 import { gameMatrixSelector } from "../store/selectors";
 import styles from "../styles/matrix.module.scss";
@@ -8,19 +7,30 @@ type Props = { className?: string };
 export default function Matrix({ className }: Props) {
   const matric = useSelector(gameMatrixSelector);
   return (
-    <div className={`${styles.container} ${className || ''}`}>
-      {matric.map((id, index) => (
-        <div
-          key={id}
-          style={{
-            "--row": Math.floor(index / 4) as number,
-            "--col": (index % 4) as number,
-          } as React.CSSProperties}
-          data-is-empty={id === 0}
-        >
-          {id !== 0 &&<span>{id}</span>}
-        </div>
-      ))}
+    <div className={`${styles.container} ${className || ""}`}>
+      {matric
+        .map((id, index) => ({
+          id,
+          row: Math.floor(index / 4),
+          col: index % 4,
+        }))
+        .sort((a, b) => a.id - b.id)
+        .map(({ id, row, col }) =>
+          id !== 0 ? (
+            <div
+              key={id}
+              style={
+                {
+                  "--row": row,
+                  "--col": col,
+                } as React.CSSProperties
+              }
+              data-is-empty={id === 0}
+            >
+              <span>{id}</span>
+            </div>
+          ) : undefined,
+        )}
     </div>
   );
 }
